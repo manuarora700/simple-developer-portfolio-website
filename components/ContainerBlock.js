9caacaa87ae6f29efd971a42d365f7d44477373c
Manu Arora <manuarorawork@gmail.com>
2021-08-01 20:13:03 +0530

    Change class to className

--- components/ContainerBlock.js
+++ components/ContainerBlock.js
@@ -43,7 +43,7 @@ export default function ContainerBlock({ children, ...customMeta }) {
           <meta property="article:published_time" content={meta.date} />
         )}
       </Head>
-      <main class="dark:bg-gray-800 w-full">
+      <main className="dark:bg-gray-800 w-full">
         <Navbar />
         <div>{children}</div>
         <Footer />
