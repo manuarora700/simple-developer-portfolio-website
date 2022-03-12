d8aaebb5400602ee2d5682e48494ee7aec8030ad
mei-yang <abbyyang66@gmail.com>
2022-03-13 10:35:06 +1300

    change title

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,8 @@ import userData from "@constants/data";
 export default function Home({ repositories }) {
   return (
     <ContainerBlock
-      title="Manu Arora - Developer, Writer, Creator"
-      description="This is a template built specifically for my blog - Creating a developer portfolio that gets you a job."
+      title="Mei Yang - Developer"
+      description="People-oriented software developer with passion, curiosity and urge to share and learn."
     >
       <Hero />
       <FavouriteProjects />
