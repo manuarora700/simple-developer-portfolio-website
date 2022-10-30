760acd94c01143f49ab122a736160e0797e9dd83
Mengsroin Heng <hengmengsroin@gmail.com>
2022-10-30 10:00:28 +0700

    update dep

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["media.istockphoto.com", "travona-api.myoptistech.com"],
+  },
+};
+
+module.exports = nextConfig;
