93411f76b4de743f999683d465b681a170d43184
gionare <gionare93@gmail.com>
2023-05-02 00:53:01 -0400

    lets see if giosbank.png will appear now

--- constants/data.js
+++ constants/data.js
@@ -37,16 +37,16 @@ const userData = {
       link: "https://play.placeholdertech.in",
       imgUrl: "/playground.png",
     },
-    {
-      title: "Feedmeback",
-      link: "https://feedmeback.vercel.app",
-      imgUrl: "/feedmeback.png",
-    },
-    {
-      title: "Devmedium",
-      link: "https://devmedium.vercel.app",
-      imgUrl: "/devmedium.png",
-    },
+    // {
+    //   title: "Feedmeback",
+    //   link: "https://feedmeback.vercel.app",
+    //   imgUrl: "/feedmeback.png",
+    // },
+    // {
+    //   title: "Devmedium",
+    //   link: "https://devmedium.vercel.app",
+    //   imgUrl: "/devmedium.png",
+    // },
   ],
   about: {
     title:
